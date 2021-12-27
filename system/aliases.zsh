57c567a3e6ab3212f45d1ccde431ec35667dc9a2
David Brownman <beamneocube@gmail.com>
2021-12-27 12:49:31 -0800

    add better error handling to a function

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -31,8 +31,14 @@ alias hoist="find . -mindepth 2 -type f -exec mv {} . \;"
 alias ng="ngrok http --subdomain xavdid"
 
 function audiobook()
-{
+{(
+  # parens so the whole shell doesn't die on error
+  if [ $# -eq 0 ]; then
+      echo "ERR: Pass the path to the .odm file as the first (and only) arg"
+      exit 1
+  fi
+  set -e
   odmpy dl "$1" --downloaddir ~/Downloads/audiobooks -mc --mergeformat m4b --nobookfolder
   echo -n "odmpy ret $1" | pbcopy
   echo "\nDone! The command to return the loan has been copied to the clipboard."
-}
+)}
