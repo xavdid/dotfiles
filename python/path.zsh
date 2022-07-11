610564bcd593d528e1689f55a803847d8b316461
David Brownman <beamneocube@gmail.com>
2022-07-11 09:43:06 -0700

    add pipx path check

--- python/path.zsh
+++ python/path.zsh
@@ -1,7 +1,7 @@
-# i'm not positive I need this? I'm on 3.8
-
-# export PATH="/Users/david/Library/Python/3.7/bin:$PATH"
-
+if command -v pipx 1>/dev/null 2>&1; then
+  # list pipx bins
+  export PATH="$PATH:$HOME/.local/bin"
+fi
 
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init --path)"
