1dcb41b876b5ad8a8331937bee7fed38d9fd06a0
David Brownman <beamneocube@gmail.com>
2022-07-22 22:28:02 -0700

    update ruby configuration

--- ruby/path.zsh
+++ ruby/path.zsh
@@ -1,4 +1,1 @@
-# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
-export PATH="$PATH:$HOME/.rvm/bin"
-
-# export PATH="/usr/local/opt/ruby@2.6/bin:/usr/local/opt/ruby/bin:$PATH"
+eval "$(rbenv init - zsh)"
