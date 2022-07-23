1975427d277228e40e8c2b1ee534678c571ba5c0
David Brownman <beamneocube@gmail.com>
2022-07-22 23:06:48 -0700

    check when using dropbox paths

--- system/path.zsh
+++ system/path.zsh
@@ -1,13 +1,1 @@
 export PATH="$DOTFILES/bin:$PATH"
-
-# export DROPBOX="$HOME/Dropbox"
-
-# elif [[ $LOC = 'TBP' ]]; then
-
-
-#     export PATH="$PATH:$DROPBOX/Saves/bin"
-
-
-# if [[ -z "$DROPBOX" ]]; then
-#     echo '!!warning!! $DROPBOX is not set'
-# fi
