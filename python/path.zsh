05557f1f5fa8f37781a06b05d829c874132d589f
David Brownman <beamneocube@gmail.com>
2022-07-26 22:35:08 -0700

    remove poetry-specific code

--- python/path.zsh
+++ python/path.zsh
@@ -8,7 +8,3 @@ if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
 
-if command -v poetry 1>/dev/null 2>&1; then
-  source $HOME/.poetry/env
-  export PATH="$HOME/.poetry/bin:$PATH"
-fi
