265e3bfb7e8355780e9475ab67651dbb629d0cac
David Brownman <beamneocube@gmail.com>
2023-03-19 12:30:37 -0700

    jk, simplify python packaging, wtf

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,3 +1,15 @@
 alias py="ptpython" # nice interpreter
 alias py3="python3 -m ptpython"
 alias http="python -m http.server"
+
+function venv() {
+  if [[ ! -d .venv ]];then
+    python -m venv .venv
+  fi
+
+  source ./.venv/bin/activate
+}
+alias pir="venv && pip install -r requirements.txt"
+alias pdump="pip freeze > requirements.txt"
+
+# or, just use poetry
