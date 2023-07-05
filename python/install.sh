1d2a1dfbe0aa1456681c44703e8e0e0bc11dda28
David Brownman <beamneocube@gmail.com>
2023-07-05 00:22:21 -0700

    update brewfile and python installer

--- python/install.sh
+++ python/install.sh
@@ -1,9 +1,13 @@
 # !/bin/zsh
 
-if test ! $(which poetry)
-then
-  echo "  installing poetry"
-  curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-else
-  echo 'poetry already installed!'
-fi
+# if test ! $(which poetry)
+# then
+#   echo   installing poetry
+#   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+# else
+#   echo 'poetry already installed!'
+# fi
+
+# python-based tools I use regularly
+# might be able to replace black w/ ruff
+pipx install black datasette hacker-news-to-sqlite litecli odmpy ptpython reddit-user-to-sqlite sqlite-utils tox universal-test-runner
