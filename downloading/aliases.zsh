5707967fb245e393a3f3ba8303a302d776e91618
David Brownman <beamneocube@gmail.com>
2023-12-30 22:06:32 -0800

    add cash-grab script

--- downloading/aliases.zsh
+++ downloading/aliases.zsh
@@ -1,4 +1,9 @@
-
 function song() {
-    youtube-dl "$1" -x --audio-format mp3 -o '%(title)s.%(ext)s'
+    yt-dlp "$1" -x --audio-format mp3 -o '%(title)s.%(ext)s'
+}
+
+function cash-grab() {
+    # call with: cash-grab "full url" "ep #" "title", e.g.
+    # cash-grab "https://www.patreon.com/posts/cash-grab-16-and-92958912" 16 "Natalie Palamides and her many bees"
+    yt-dlp --cookies-from-browser firefox "$1" -o "$DROPBOX/Archive/Videos/cashgrab/Cash Grab - Episode $2 - $3.%(ext)s"
 }
