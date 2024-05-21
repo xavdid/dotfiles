c4bd156ee9330034ba1ab0d40690c80e338edcc0
David Brownman <beamneocube@gmail.com>
2024-05-20 20:41:39 -0700

    update venv command to support other names

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -3,12 +3,20 @@ alias py3="python3 -m ptpython"
 alias http="python -m http.server"
 
 function venv() {
-  if [[ ! -d .venv ]];then
-    python -m venv .venv
-  fi
+  possible_names=('.venv' 'venv')
+  for name in ${possible_names[*]};
+  do
+    if [[ -d $name ]];then
+      source ./$name/bin/activate
+      return
+    fi
+  done
 
+  # If it gets here, none of the possible names exist, so create and activate `.venv`
+  python -m venv .venv
   source ./.venv/bin/activate
 }
+
 alias pir="venv && pip install -r requirements.txt"
 alias pdump="pip freeze > requirements.txt"
 
