803282457f0c859dc5b8b9bf01488e2cc3202c31
David Brownman <beamneocube@gmail.com>
2024-05-24 21:33:17 -0700

    swap dev tools to mise

--- python/path.zsh
+++ python/path.zsh
@@ -2,12 +2,3 @@ if command -v pipx 1>/dev/null 2>&1; then
   # list pipx bins
   export PATH="$PATH:$HOME/.local/bin"
 fi
-
-if command -v pyenv 1>/dev/null 2>&1; then
-  eval "$(pyenv init --path)"
-  eval "$(pyenv init -)"
-fi
-
-if which pyenv-virtualenv-init > /dev/null; then
-  eval "$(pyenv virtualenv-init -)"
-fi
