8325ca37830628f18715d3e9dec5169cc2befd36
David Brownman <oss@xavd.id>
2025-06-16 18:17:41 -0700

    fix patreon downloading

--- downloading/aliases.zsh
+++ downloading/aliases.zsh
@@ -5,5 +5,5 @@ function song() {
 function cash-grab() {
     # call with: cash-grab "full url" "ep #" "title", e.g.
     # cash-grab "https://www.patreon.com/posts/cash-grab-16-and-92958912" 16 "Natalie Palamides and her many bees"
-    yt-dlp --cookies-from-browser firefox "$1" -o "$DROPBOX/Archive/Videos/cashgrab/Cash Grab - Episode $2 - $3.%(ext)s"
+    yt-dlp --referer 'https://www.patreon.com/' --cookies-from-browser firefox "$1" -o "$DROPBOX/Archive/Videos/cashgrab/Cash Grab - Episode $2 - $3.%(ext)s"
 }
