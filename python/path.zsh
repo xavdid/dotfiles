a61f3a387fd7029fa61ac9516228ca3b43f58480
David Brownman <oss@xavd.id>
2026-09-22 22:26:58 -0700

    re-add uv tools to path

--- python/path.zsh
+++ python/path.zsh
@@ -1,7 +1,5 @@
-# if command -v pipx 1>/dev/null 2>&1; then
-#   # list pipx bins
-#   export PATH="$PATH:$HOME/.local/bin"
-# fi
+# uv tools
+export PATH="/Users/david/.local/bin:$PATH"
 
 # don't allow global pip
 export PIP_REQUIRE_VIRTUALENV=1
